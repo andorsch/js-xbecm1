838449a250781f3c6932b652e1400de0de70e9aa
andorsch <andor.schvarcz@gmail.com>
2021-02-23 18:32:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-xbecm1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-xbecm1)
---
+++ index.js
@@ -0,0 +1,47 @@
+// Write Javascript code!
+
+// 1 What is the result?
+
+// console.log(a);
+
+// var a = 2;
+
+// 2 What is the result?
+
+// console.log(b);
+
+// const b = 2;
+
+// 3 Which will work?
+
+// doSomething();
+
+// function doSomething() {
+//   let x = "Something";
+//   console.log(x);
+// }
+
+// doSomething()
+
+// 5 Will this work?
+
+// function doSomething() {
+//   let x = "Something";
+//   console.log(x);
+// }
+
+// console.log(x)
+
+// 6 What is the result?
+
+// function work(a) {
+//   if (a == 0) {
+//     return a;
+//   } else {
+//     return work(a - 1);
+//   }
+// }
+
+// (a => {
+//   console.log(work(a));
+// })(10);
